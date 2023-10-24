9630a54a2c432d04ff30c0bed399f6b9aeec1849
eulia16 <ethan.uliano@gmail.com>
2023-10-24 19:19:56 -0400

    C++ version done i think right config name

--- HW_3/main.cpp
+++ HW_3/main.cpp
@@ -24,12 +24,8 @@ int main() {
         //if we want to have one network tick present as expected
         for(int i=0; i<3; ++i){
 
-            //incase you want the states of each model as well
-//                System.out.println("Information regarding each atomic model");
-//                Thread.sleep(1000);
-//                System.out.println("XOR1 state: " + networkModel.getXOR2().lamdba());
-//                System.out.println("XOR2 state: " + networkModel.getXOR2().lamdba());
-//                System.out.println("Memory Model state: " + networkModel.getMemoryModel().lambda());
+            //if we want to print out all the values in the middle we can upon request
+
             //calls the labmdas of each model
             networkModel->couplings();
             networkModel->delta(input);
